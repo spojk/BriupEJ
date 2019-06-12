d60f79a3351704648b6f2e6fc890877082a6ad16
CodingFanlt <CodingFanlt@Gmail.com>
2019-06-12 08:53:10 +0800

    Initial commit

---
+++ src/main/java/com/briup/apps/briupej/BriupEjApplication.java
@@ -0,0 +1,14 @@
+package com.briup.apps.briupej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BriupEjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BriupEjApplication.class, args);
+    }
+
+}
+
